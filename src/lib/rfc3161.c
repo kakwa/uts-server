0ba0c0914d6c168b30dabc4a82e543e2881b14a4
pcarpent <pierre-francois.carpentier@c-s.fr>
2015-12-17 14:09:44 +0100

    begin implementing

---
+++ src/lib/rfc3161.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/rand.h>
+#include <openssl/ts.h>
+#include <openssl/bn.h>
+
