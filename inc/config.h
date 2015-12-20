8c57bcd0a0e6e8d45850fc2aa3e83826264433c8
Pierre-Francois Carpentier <carpentier.pf@gmail.com>
2015-12-20 13:58:42 +0100

    adding configuration structure

---
+++ inc/config.h
@@ -0,0 +1,13 @@
+typedef struct uts_config {
+    char * port;
+    char * listen;
+    bool https;
+    cert https_cert;
+    void * ts_certs;
+
+} uts_config;
+
+typedef struct cert {
+    char * cert_file;
+    char * key_file;
+} cert;
