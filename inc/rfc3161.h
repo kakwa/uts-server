d104de63d5769e2ac03df4b99e7bdff192eb2154
kakwa <carpentier.pf@gmail.com>
2016-09-02 08:00:04 +0200

    adding a function to free the ssl context

--- inc/rfc3161.h
+++ inc/rfc3161.h
@@ -39,3 +39,4 @@ TS_RESP_CTX *create_tsctx(rfc3161_context *ct, CONF *conf, const char *section,
                           const char *policy);
 int add_oid_section(rfc3161_context *ct, CONF *conf);
 void init_ssl();
+void free_ssl();
