e2e254480afc86651ffd4f1352d145585051583b
kakwa <carpentier.pf@gmail.com>
2016-09-07 21:43:15 +0200

    small improvement on test script

--- tests/external_test.sh
+++ tests/external_test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-timeout 20 ./uts-server -c tests/cfg/uts-server.cnf -D &
+timeout 120 ./uts-server -c tests/cfg/uts-server.cnf -D -p ./uts-server.pid &
 
 ./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
 ./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
 ./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
+
+kill `cat ./uts-server.pid`
 
