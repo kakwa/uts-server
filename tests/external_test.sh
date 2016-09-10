0278c1e65f409f88823fd330cff4397c6b1997d3
kakwa <carpentier.pf@gmail.com>
2016-09-10 03:14:02 +0200

    fix tests

--- tests/external_test.sh
+++ tests/external_test.sh
@@ -2,9 +2,9 @@
 
 timeout 120 ./uts-server -c tests/cfg/uts-server.cnf -D -p ./uts-server.pid &
 
-./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
-./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
-./goodies/timestamp-file.sh -i README.md -u http://localhost:2020 -r -O "-cert" || exit 1
+./goodies/timestamp-file.sh -i README.rst -u http://localhost:2020 -r -O "-cert" || exit 1
+./goodies/timestamp-file.sh -i README.rst -u http://localhost:2020 -r -O "-cert" || exit 1
+./goodies/timestamp-file.sh -i README.rst -u http://localhost:2020 -r -O "-cert" || exit 1
 
 kill `cat ./uts-server.pid`
 
