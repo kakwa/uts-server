7162b59690a7363b80642d6f298c0104733d380e
kakwa <carpentier.pf@gmail.com>
2016-09-11 23:59:52 +0200

    adding test case for ssl setup

--- tests/external_test.sh
+++ tests/external_test.sh
@@ -9,3 +9,13 @@ sleep 1
 
 kill `cat ./uts-server.pid`
 
+
+timeout 120 ./uts-server -c tests/cfg/uts-server-ssl.cnf -D -p ./uts-server.pid &
+
+sleep 1
+./goodies/timestamp-file.sh -i README.rst -u https://localhost:2020 -r -O "-cert" -C '-k' || exit 1
+./goodies/timestamp-file.sh -i README.rst -u https://localhost:2020 -r -O "-cert" -C '-k' || exit 1
+./goodies/timestamp-file.sh -i README.rst -u https://localhost:2020 -r -O "-cert" -C '-k' || exit 1
+
+kill `cat ./uts-server.pid`
+
