e019d48216e1d3e5ac47770c94a873d86128b6ea
kakwa <carpentier.pf@gmail.com>
2017-04-27 22:40:21 +0200

    fix test script for OpenBSD

--- tests/external_test.sh
+++ tests/external_test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-timeout 120 ./uts-server -c tests/cfg/uts-server.cnf -D -p ./uts-server.pid &
+if which timeout >/dev/null 2>&1
+then
+    TO="timeout 120"
+else
+    TO=""
+fi
+
+$TO ./uts-server -c tests/cfg/uts-server.cnf -D -p ./uts-server.pid &
 
 sleep 1
 ./goodies/timestamp-file.sh -i README.rst -u http://localhost:2020 -r -O "-cert" || exit 1
@@ -8,13 +15,6 @@ sleep 1
 ./goodies/timestamp-file.sh -i README.rst -u http://localhost:2020 -r -O "-cert" || exit 1
 
 kill `cat ./uts-server.pid`
-
-if which timeout >/dev/null 2>&1
-then
-    TO="timeout 120"
-else
-    TO=""
-fi
 
 $TO ./uts-server -c tests/cfg/uts-server-ssl.cnf -D -p ./uts-server.pid &
 
@@ -24,4 +24,3 @@ sleep 1
 ./goodies/timestamp-file.sh -i README.rst -u https://localhost:2020 -r -O "-cert" -C '-k' || exit 1
 
 kill `cat ./uts-server.pid`
-
